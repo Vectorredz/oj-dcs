145210003deb6a78d0144153194a0a489a16e691
Vectorredz <louisegabrielvilar69@gmail.com>
2025-09-04 11:19:01 +0800

    chore: testo

--- CS-33/lab2/lec3/test.py
+++ CS-33/lab2/lec3/test.py
@@ -1,21 +1,18 @@
 from heapq import heappush, heappop
 from collections.abc import Sequence
-from collections import defaultdict
 type Teleporter = tuple[int, int]
 
 def max_grunts(g: Sequence[int], teleporters: Sequence[Teleporter]) -> int:
     n = len(g)
 
-    # 1. Build adjacency list
+    # Build adjacency list
     adj = [[] for _ in range(n)]
     for u, v in teleporters:
         adj[u-1].append(v-1)
 
-    # --- 2. init variables: order, visited ---
+    # --- Kosaraju to compress SCCs ---
     visited = [False]*n
     order = []
-    
-    # 3. do dfs 1: store order after for
 
     def dfs1(u):
         visited[u] = True
@@ -27,58 +24,44 @@ def max_grunts(g: Sequence[int], teleporters: Sequence[Teleporter]) -> int:
     for i in range(n):
         if not visited[i]:
             dfs1(i)
-            
-    # 4. reverse the edges and do dfs2
-    
+
     rev_adj = [[] for _ in range(n)]
     for u, v in teleporters:
         rev_adj[v-1].append(u-1)
 
-    visited = [False]*n
-    components = []
-
-    def dfs2(u, comp):
-        visited[u] = True
-        comp.append(u)
+    comp = [-1]*n
+    def dfs2(u, cid):
+        comp[u] = cid
         for v in rev_adj[u]:
-            if not visited[v]:
-                dfs2(v, comp)
-                
-    # 5. pop the order
-    for u in reversed(order):
-        if not visited[u]:
-            # init the comp every iter
-            comp = []
-            dfs2(u, comp)
-            # store the comp at components
-            components.append(comp)
+            if comp[v] == -1:
+                dfs2(v, cid)
 
-    # Now assign component IDs with enumerate
-    comp_id = [-1]*n
-    for cid, comp in enumerate(components):
-        for u in comp:
-            comp_id[u] = cid
+    cid = 0
+    for u in reversed(order):
+        if comp[u] == -1:
+            dfs2(u, cid)
+            cid += 1
 
     # Build SCC DAG
-    scc_val = defaultdict(int)
+    scc_val = [0]*cid
     for u in range(n):
-        scc_val[comp_id[u]] += g[u]
+        scc_val[comp[u]] += g[u]
 
-    scc_adj = [[] for _ in range(len(components))]
+    scc_adj = [[] for _ in range(cid)]
     for u, v in teleporters:
-        cu, cv = comp_id[u-1], comp_id[v-1]
+        cu, cv = comp[u-1], comp[v-1]
         if cu != cv:
             scc_adj[cu].append(cv)
-    
-    print(scc_adj)
 
-    # --- "Dijkstra" on DAG (max path DP with PQ) ---
-    dist = [-10**18]*len(components)
+    # --- "Dijkstra" on DAG (actually max path DP with PQ) ---
+    dist = [-10**18]*cid
     pq = []
 
-    for i in range(len(components)):
+    # Start from each SCC individually
+    for i in range(cid):
         dist[i] = scc_val[i]
         heappush(pq, (-dist[i], i))
+
     ans = 0
     while pq:
         curr_neg, u = heappop(pq)
